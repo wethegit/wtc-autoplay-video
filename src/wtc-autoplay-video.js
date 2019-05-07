175419ecc62d63a4fc1aa97df7e74d068681212c
Liam Egan <liam@wethecollective.com>
2019-05-07 16:03:57 -0700

    Adding basic documentation

--- src/wtc-autoplay-video.js
+++ src/wtc-autoplay-video.js
@@ -10,7 +10,105 @@ const defaults = {
   loopTo: null
 };
 
+/**
+ * The AutoplayVideo class expects an element (as all wtc-element-controllers do) and can take an optional options argument
+ * ## Install
+ * ```sh
+ * $ npm install wtc-autoplay-video
+ * ```
+ * ## Usage
+ * Import it in your project.
+ * ```javascript
+ * import AutoplayVideo from 'wtc-autoplay-video';
+ * ```
+ * Add the markup.
+ * ```html
+ * <div class="autoplay-video">
+ *   <video
+ *     class="autoplay-video__video"
+ *     autoplay
+ *     playsinline
+ *     muted
+ *     loop
+ *   >
+ *     <source
+ *       src="video.mp4"
+ *       type="video/mp4"
+ *     />
+ *   </video>
+ *   <!-- REQUIRED -->
+ *   <img
+ *     class="autoplay-video__fallback"
+ *     src="fallback.png"
+ *     alt="Fallback"
+ *   />
+ *   <!-- OPTIONAL -->
+ *   <img
+ *     class="autoplay-video__preloader"
+ *     src="preloader.png"
+ *     alt="Preloader"
+ *   />
+ * </div>
+ * ```
+
+ * ### 1. Using ExecuteControllers
+ * If you are using [wtc-controller-element] just **data-controller="AutoplayVideo"** to your markup.
+ * ```html
+ * <div data-controller="AutoplayVideo" class="autoplay-video">
+ * ...
+ * ```
+ * You can also instanciate explicitly:
+ * ```javascript
+ * ExecuteControllers.instanciate(document.getElementById('autoplay-video'), AutoplayVideo);
+ * ```
+ * ### 2. Default JS
+ * With the default js version, you have the option to pass the options as an object, or use data-attributes, they both work.
+ * ```javascript
+ * let gallery = new AutoplayVideo(document.getElementById('autoplay-video'), {
+ *   fullWidth: false,
+ *   vpOn: 30
+ * });
+ * ```
+ * ### 3. ES5 version
+ * There's also an ES5 version to be used in browser anywhere. It's also really simple.
+ * Add you markup, then add the script:
+ * ```html
+ * <script src="dist/wtc-autoplay-video.es5.js"></script>
+ * ```
+ * And for last, instanciate the videos:
+ * ```javascript
+ * <script>
+ *   var videos = document.querySelectorAll('.autoplay-video');
+ *   for (var i = 0; i < videos.length; i++) {
+ *     new WTCAutoplayVideo.default(videos[i], { fullWidth: false, vpOn: 0 });
+ *   }
+ * </script>
+ * 
+ * @class AutoplayVideo
+ * @augments Viewport
+ * @returns A new instance of the AutoplayVideo class
+ * @author Liam Egan <liam@wethecollective.com>
+ * @version 1.0.0
+ * @created May 7, 2019
+ */
 class AutoplayVideo extends Viewport {
+  /**
+   * Creates an instance of AutoplayVideo.
+   * 
+   * Options object is comprised of the following:
+   * | Name | Type | Description | Default |
+   * | ---- | ---- | ----------- | _______ |
+   * | fullWidth | `Boolean`  | Whether the video should display fullscreen | false |
+   * | vpOn | `Number`  | The point at which the video should start playing after havign scrolled on the screen. | 0 |
+   * | startAt | `Number`  | When the video starts playing again, start at this point, in seconds. | null |
+   * | loopFrom | `Number`  | When the video reaches this part, loop. If this isn't provided, the end of the video will be the loop point. | null |
+   * | loopTo | `Number`  | When the video loops, this is the point that it will start the loop from. | null |
+   * 
+   * @constructor
+   * @param {DOMElement} element 
+   * @param {Object} options 
+   * @memberOf AutoplayVideo
+   */
   constructor(element, options) {
     super(element);
 
@@ -56,7 +154,6 @@ class AutoplayVideo extends Viewport {
     } else if (this._video.readyState >= 2) {
       this.init();
     } else {
-      // this._video.addEventListener('canplay', this.init.bind(this), false);
       this.init();
     }
     
@@ -73,6 +170,13 @@ class AutoplayVideo extends Viewport {
     this.animationCallback = this.viewportAnimationCallback.bind(this);
   }
 
+  /**
+   * This method initialises the loaded video, sets up our ratios and attaches the 
+   * relevant event listeners.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   init() {
     if (!this.initiated) {
       this.initiated = true;
@@ -98,6 +202,13 @@ class AutoplayVideo extends Viewport {
     }
   }
 
+  /**
+   * Resize the video to the size of its parent. This is normally called as a part 
+   * of the window resize handler but can also be called programatically.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   videoResize() {
     let targetW = this._video.parentNode.offsetWidth,
       targetH = this._video.parentNode.offsetHeight,
@@ -118,6 +229,13 @@ class AutoplayVideo extends Viewport {
     this._video.style.width = newW + "px";
   }
 
+  /**
+   * Responds to the videos playing method. This is responsible for setting the various 
+   * state properties and starting up the run loop, if we need internal video looping.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   onPlay() {
     this.element.classList.add('is-playing');
     this.hasStarted = true;
@@ -127,22 +245,55 @@ class AutoplayVideo extends Viewport {
     }
   }
 
+  /**
+   * If for some reason the video fails to play, this method will be called.
+   * This adds an `is-frozen` class name to the element and sets `videoPlaying`
+   * property to false.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   onFrozen() {
     this.element.classList.add('is-frozen');
     this.videoPlaying = false;
   }
 
+  /**
+   * When the video is paused this method is called.
+   * This adds the `is-paused` class name to the element, sets the `hasStarted`
+   * and `videoPlaying` properties to false.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   onPause() {
     this.element.classList.add('is-paused');
     this.hasStarted = false;
     this.videoPlaying = false;
   }
 
+  /**
+   * This responds to the videos `ended` event and is responsible for looping
+   * the video only when a both a `loopTo` property is provided and a `loopFrom`
+   * property is *not*.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   onEnded() {
     this._video.currentTime = this.options.loopTo;
     this._video.play();
   }
 
+  /**
+   * This method is a part of the run loop for the video. It will only run when
+   * the video is playing (on screen) and when both the `loopTo` and `loopFrom`
+   * properties are provided
+   * 
+   * @public
+   * @param {Number} delta 
+   * @memberOf AutoplayVideo
+   */
   onLoopCheck(delta) {
     if(this.videoPlaying === true && this.loopPeriod === true) {
       requestAnimationFrame(this.onLoopCheck);
@@ -153,11 +304,23 @@ class AutoplayVideo extends Viewport {
     }
   }
 
+  /**
+   * This method pauses the video and is intended to be called programatically.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   pauseVideo() {
     this._video.pause();
     this.onPause();
   }
 
+  /**
+   * This method plays the video and is intended to be called programatically.
+   * 
+   * @public
+   * @memberOf AutoplayVideo
+   */
   playVideo() {
     if (this._video.readyState >= 2) {
       if (this._video.paused) {
@@ -182,6 +345,16 @@ class AutoplayVideo extends Viewport {
     }
   }
 
+  /**
+   * This method overrides the parent class' viewportAnimationCallback
+   * method and provides play/pause functionality based on the viewport
+   * position. Basically this stops the video from playing unless it's
+   * on-screen. 
+   * 
+   * @override Viewport.viewportAnimationCallback
+   * @param {any} topPercent 
+   * @memberOf AutoplayVideo
+   */
   viewportAnimationCallback(topPercent) {
     if (!this.isOnScreen && this.hasStarted && !this._video.paused) {
       this.pauseVideo();
